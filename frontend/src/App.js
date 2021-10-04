c28035e23adbb86ee1242b8e3fea1f33ea34eec6
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-04 10:03:12 -0400

    Working on react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap'
 
 function App() {
+  const [bookName, setBookName] = useState('')
+  const [review, setReview] = useState('')
+  const [fetchData, setFetchData] = useState('')
+  const [reviewUpdate, setReviewUpdate] = useState('')
+
+  const handleBookChange = (event) => {
+    const name = event.target.name
+    setBookName(name)
+  }
+
+  const handleReviewChange = (event) => {
+    const review = event.target.review
+    setReview(review)
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:4005/get')
+    .then((res) => {
+      setFetchData(res.data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
